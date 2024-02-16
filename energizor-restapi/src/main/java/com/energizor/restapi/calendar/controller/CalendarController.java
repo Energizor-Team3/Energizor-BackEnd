e2dfa8c7bc8d023ec1619c016b491290357b1692
sooyeun-kim <sueyeon777@gmail.com>
2024-02-16 11:22:37 +0900

    2/16 스웨거 연동

--- energizor-restapi/src/main/java/com/energizor/restapi/calendar/controller/CalendarController.java
+++ energizor-restapi/src/main/java/com/energizor/restapi/calendar/controller/CalendarController.java
@@ -9,6 +9,8 @@ import com.energizor.restapi.calendar.service.CalendarService;
 import com.energizor.restapi.calendar.service.ScheduleService;
 import com.energizor.restapi.common.ResponseDTO;
 import com.energizor.restapi.users.dto.UserDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +18,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-//@Tag(name= "캘린더 관련 스웨거 연동 ")
+@Tag(name= "캘린더 관련 스웨거 연동 ")
 @RestController
 @RequestMapping("/calendar")
 @Slf4j
@@ -35,7 +37,7 @@ public class CalendarController {
 //----------------------------------------** 캘린더 **-----------------------------------------------------
 //    캘린더 정보 조회
 
-//    @Operation(summary = "캘린더 정보 조회 ",description = " 하나의 캘린더 정보를 조회한다 ")
+    @Operation(summary = "캘린더 정보 조회 ",description = " 하나의 캘린더 정보를 조회한다 ")
     @GetMapping("/calendar/{calNo}")
     public ResponseEntity<ResponseDTO> getCalendar(@PathVariable int calNo) {
 
@@ -43,14 +45,14 @@ public class CalendarController {
     }
 
 //    캘린더 전체 목록 조회
-//    @Operation(summary = "전체 캘린더 조회 ",description = " 모든 캘린더 정보를 조회한다 ")
+    @Operation(summary = "전체 캘린더 조회 ",description = " 모든 캘린더 정보를 조회한다 ")
     @GetMapping("/calendars")
     public ResponseEntity<ResponseDTO> getAllCalendars() {
         return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "모든 캘린더 정보 조회 성공", calendarService.findAllCalendars()));
     }
 
 //    캘린더 타입으로 조회
-//    @Operation(summary = "캘린더 타입으로 조회 ",description = " 캘린더 타입별로 조회를 한다  ")
+    @Operation(summary = "캘린더 타입으로 조회 ",description = " 캘린더 타입별로 조회를 한다  ")
     @GetMapping("/calendarByType/{calType}")
     public ResponseEntity<ResponseDTO> getCalendarsByType(@PathVariable String calType) {
         return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "캘린더 정보 조회 성공", calendarService.findCalendarsByType(calType)));
@@ -64,7 +66,7 @@ public class CalendarController {
     }
 
 // 로그인한 유저 코드로 캘린더 추가
-//@Operation(summary = "캘린더 추가  ",description = " 로그인한 유저의 캘린더를 추가한다  ")
+@Operation(summary = "캘린더 추가  ",description = " 로그인한 유저의 캘린더를 추가한다  ")
 @PostMapping("/addCalendar")
 public ResponseEntity<ResponseDTO> addNewCalendar(@RequestBody CalendarAndParticipantDTO calendarAndParticipantDTO,
                                                   @AuthenticationPrincipal UserDTO principal) {
@@ -84,7 +86,7 @@ public ResponseEntity<ResponseDTO> addNewCalendar(@RequestBody CalendarAndPartic
 
 
     // 캘린더 수정
-//    @Operation(summary = "캘린더 수정  ",description = " 로그인한 유저의 캘린더를 수정한다  ")
+    @Operation(summary = "캘린더 수정  ",description = " 로그인한 유저의 캘린더를 수정한다  ")
     @PatchMapping("/updateCalendar/{calNo}")
     public ResponseEntity<ResponseDTO> updateCalendar(@PathVariable int calNo,
                                                       @RequestBody CalendarAndParticipantDTO calendarAndParticipantDTO,
@@ -116,7 +118,7 @@ public ResponseEntity<ResponseDTO> addNewCalendar(@RequestBody CalendarAndPartic
 // 캘린더 삭제
 
 
-//@Operation(summary = "캘린더 삭제  ",description = " 로그인한 유저의 캘린더 하나를 삭제한다  ")
+@Operation(summary = "캘린더 삭제  ",description = " 로그인한 유저의 캘린더 하나를 삭제한다  ")
 @DeleteMapping("/deleteCalendar/{calNo}")
 public ResponseEntity<ResponseDTO> deleteCalendar(@PathVariable int calNo,
                                                   @AuthenticationPrincipal UserDTO principal) {
@@ -138,19 +140,19 @@ public ResponseEntity<ResponseDTO> deleteCalendar(@PathVariable int calNo,
 
 
 // 캘린더 코드로 해당 캘린더 일정 조회
-//@Operation(summary = "캘린더 일정 조회  ",description = " 캘린더 코드 별 일정을 조회한다  ")
+@Operation(summary = "캘린더 일정 조회  ",description = " 캘린더 코드 별 일정을 조회한다  ")
     @GetMapping("/schedule/{calNo}")
     public ResponseEntity<ResponseDTO> getScheduleByCalendarCode(@PathVariable int calNo) {
         return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,  calNo + "번 캘린더의 스케줄 조회 성공", scheduleService.findSchedulesByCalNo(calNo)));
     }
-//    @Operation(summary = "일정 추가  ",description = " 일정을 추가 한다 ")
+    @Operation(summary = "일정 추가  ",description = " 일정을 추가 한다 ")
     @PostMapping("/schedule/insert")
     public ResponseEntity<ResponseDTO> insertSchedule(@RequestBody ScheduleDTO scheduleDTO) {
 
         return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"스케줄 등록 성공",scheduleService.insertSchedule(scheduleDTO)));
 
     }
-//    @Operation(summary = "일정 삭제  ",description = " 일정을 삭제한다 ")
+    @Operation(summary = "일정 삭제  ",description = " 일정을 삭제한다 ")
     @DeleteMapping("/schedule/delete/{schNo}")
     public ResponseEntity<ResponseDTO> deleteSchedule(@PathVariable int schNo) {
         String result = scheduleService.deleteSchedule(schNo);
@@ -160,7 +162,7 @@ public ResponseEntity<ResponseDTO> deleteCalendar(@PathVariable int calNo,
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDTO(HttpStatus.NOT_FOUND, "존재하지 않는 스케줄 입니다", null));
         }
     }
-//    @Operation(summary = "일정 수정  ",description = " 일정을 수정 한다 ")
+    @Operation(summary = "일정 수정  ",description = " 일정을 수정 한다 ")
     @PatchMapping("/schedule/update/{schNo}")
     public ResponseEntity<ResponseDTO> updateSchedule(@PathVariable int schNo, @RequestBody ScheduleDTO updatedScheduleDTO) {
         try {
