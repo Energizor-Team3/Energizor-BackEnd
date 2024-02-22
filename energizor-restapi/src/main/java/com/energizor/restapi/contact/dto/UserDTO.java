b4fae73162c1291d5c8ca0297739bf5243f43bb8
dev-dahee <dev_dahee@naver.com>
2024-02-22 16:12:53 +0900

    24.02.22 주소록 수정

--- energizor-restapi/src/main/java/com/energizor/restapi/contact/dto/UserDTO.java
+++ energizor-restapi/src/main/java/com/energizor/restapi/contact/dto/UserDTO.java
@@ -1,5 +1,12 @@
 package com.energizor.restapi.contact.dto;
+import com.energizor.restapi.users.dto.DayOffDTO;
+import com.energizor.restapi.users.dto.UserRoleDTO;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -10,5 +17,13 @@ public class UserDTO {
 
     private int userCode;
     private String userName;
+    private String userId;
+    private String userRank;
+    private String email;
+    private String phone;
+    private List<UserDTO> userDTO;
+
+    private String deptName;
+    private String teamName;
 
 }
