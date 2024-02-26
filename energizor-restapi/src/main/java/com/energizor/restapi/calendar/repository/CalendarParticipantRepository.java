b8dd44d1efdfbd7b38fed57294c756023f0520c6
[CHUWENCHING] <[chuuu3619@gmail.com]>
2024-02-26 09:42:26 +0900

    온청 2024-02-25 캘린더 조회 부분 업데이트

--- energizor-restapi/src/main/java/com/energizor/restapi/calendar/repository/CalendarParticipantRepository.java
+++ energizor-restapi/src/main/java/com/energizor/restapi/calendar/repository/CalendarParticipantRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface CalendarParticipantRepository extends JpaRepository<CalendarParticipant, CalendarParticipantPK> {
     List<CalendarParticipant> findByCalParticipant_UserCode(int userCode);
